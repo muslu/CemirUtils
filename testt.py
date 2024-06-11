6cafe97982eb0b45c24c17f5b0ad67443dfe0dfd
Muslu YÜKSEKTEPE <musluyuksektepe@gmail.com>
2024-06-11 13:20:06 +0300

    Delete testt.py

--- testt.py
+++
@@ -1,5 +0,0 @@
-from cemirutils import CemirUtils
-
-utils = CemirUtils(data=False, dbname='test_db3', dbuser='postgres', dbpassword='asd', dbport=5435, dbcreate_db_if_not_exists=False)
-
-utils.listen_for_icmp(print_query=True)
