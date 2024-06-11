bcb767d7e5b7426ff98f961bf7cf818eb3bbbf06
projeler <muslu.yuksektepe@makdos.com>
2024-06-11 13:11:48 +0300

    icmp/ping loglama ve db ye kayıt

--- upload2pypi.sh
+++ upload2pypi.sh
@@ -9,3 +9,6 @@ python -m twine upload dist/*
 
 
 python -m pip install -U cemirutils
+
+
+pypi-AgEIcHlwaS5vcmcCJDNkNzQ5NGEzLThmNzctNDJhZC05ZTI2LTk5NTlhNDBiMDgwYgACElsxLFsiY2VtaXJ1dGlscyJdXQACLFsyLFsiNWE1MTg1MDAtMTVmOS00MDZlLTlkNTEtYTJkZGI5NjljMDk5Il1dAAAGIBZ-_ha2PA1UcYQRXFdFwUpjMl8RG5nwcViwuNJOEzLQ
