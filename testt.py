73f774ba462c327338c9c464ba2a48b2c83a48e7
projeler <muslu.yuksektepe@makdos.com>
2024-06-14 13:24:04 +0300

    ip den ülke bilgisi ve PSQL sınıfı ayrıldı

--- testt.py
+++ testt.py
@@ -6,7 +6,7 @@ ip_geolocator = IPGeolocation()
 # ip_geolocator.create_sqlite_db()
 
 #
-ip_address = "121.0.11.0"
+ip_address = "185.122.202.210"
 # # IP adresinin lokasyon bilgisini al (Zip dosyasını tekrar indir)
 location_info = ip_geolocator.get_ip_location(ip_address, force_download=False)
 print(location_info)
